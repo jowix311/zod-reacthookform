a1074a2a071ddd98f94c3757e1c0a6e219e5fdba
jowix <jowix311@gm,ail.com>
2024-05-19 12:32:42 +0800

    Added Shadcn forms with react hook form and zod - initial logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+ * We are using ShadCN https://ui.shadcn.com/docs/components/form for the reusable components
+ * Follow ShadCN steps on initializing it on the project
+ *
+ *
+ * npm i -save-dev @hookform/resolvers for the resolver
+ */
+import "./App.css";
+
+import { z } from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const myFormSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const myForm = useForm<z.infer<typeof myFormSchema>>({
+    resolver: zodResolver(myFormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof myFormSchema>) {
+    console.log("my form values", values);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Form {...myForm}>
+      <form onSubmit={myForm.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={myForm.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
 
-export default App
+export default App;
